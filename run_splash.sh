abd06c607aad8767004c3ebc58cdb77513ccbc02
root <dennis.urrutia@usach.cl>
2024-02-12 18:31:48 -0300

    added setup.py and LICENSE for pip-instalable

--- run_splash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd outputs/snapshot_p8_n10000_a0.6_b1_tf6_e1_m0_hm0_vm0_10
-splash -f gadget_hdf5 snapshot_3d_000.0.hdf5
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mesph",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "h5py",
+        "scipy",
+        "matplotlib",
+    ],
+    # metadata to display on PyPI
+    author="Dennis Urrutia",
+    author_email="dennis.urrutia@usach.cl",
+    description="This is a program that transforms a set of outputs simulation files from FARGO3D into a SPH data, specifically to a GADGET-3 file with HDF5 format.",
+    keywords="sph, eulerian mesh, fargo3d, gadget-3, hdf5, transformation, extrapolation",
+    url="https://github.com/DennisUrr/mesph",
+    license='LICENSE',
+    scripts=['main.py', 'main_parallel.py', 'measures_error.py', 'processing.py', 'run_splash.py'],
+    long_description=open('README.md').read(),
+    include_package_data=True
+)
