981ac30d10ccb096f7ccc62df80fb7357b40f978
Dennis <dennis.urrutia@usach.cl>
2024-02-29 13:28:48 -0300

    updated workflow and setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version="1.0",
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "h5py",
-        "scipy",
-        "matplotlib",
+        "numpy>=1.21.5",
+        "h5py>=3.8.0",
+        "scipy>=1.7.3",
+        "matplotlib>=3.5.3",
     ],
     # metadata to display on PyPI
     author="Dennis Urrutia",
@@ -17,7 +17,8 @@ setup(
     keywords="sph, eulerian mesh, fargo3d, gadget-3, hdf5, transformation, extrapolation",
     url="https://github.com/DennisUrr/mesph",
     license='LICENSE',
-    scripts=['main.py', 'measures_error.py', 'run_splash.py'],
+    scripts=['main.py', 'measures_error.py'],
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     include_package_data=True
 )
